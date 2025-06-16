38f0c4b5a9c5e3a56c96e5a311e07e9cb7dd2c85
Florian GAMBARDELLA <florian.vente59@gmail.com>
2025-06-16 22:07:06 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  allowedDevOrigins: ['https://studio.firebase.google.com', 'https://6000-firebase-studio-1750027131681.cluster-axf5tvtfjjfekvhwxwkkkzsk2y.cloudworkstations.dev'],
+}
+
+module.exports = nextConfig
