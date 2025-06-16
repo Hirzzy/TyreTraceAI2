7498c9f0a679cca2081b2094c80c012b537354c5
Florian GAMBARDELLA <florian.vente59@gmail.com>
2025-06-16 20:10:18 +0000

    // firebase-config.js import { initializeApp } from "firebase/app";
    con

---
+++ src/lib/firebase-config.js
@@ -0,0 +1,14 @@
+// firebase-config.js
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "TA_CLE_API",
+  authDomain: "ton-projet.firebaseapp.com",
+  projectId: "studio-8267356228",
+  storageBucket: "ton-projet.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "1:XXXX:web:XXXX"
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
