1e0ce008cab2b8eac2946a2a79dc9c3cf8b26933
Florian GAMBARDELLA <florian.vente59@gmail.com>
2025-06-19 20:19:25 +0000

    fix (application)

--- src/app/(application)/page.tsx
+++
@@ -1,8 +0,0 @@
-// src/app/(application)/page.tsx
-import { redirect } from 'next/navigation';
-
-export default function ApplicationRootRedirectPage() {
-  redirect('/dashboard');
-  // This component will not render anything as redirect() throws an error that Next.js handles.
-  // return null; // Or an empty fragment <> </>; it won't be reached.
-}
