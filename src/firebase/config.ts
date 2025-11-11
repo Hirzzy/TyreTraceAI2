c94be73dc296e1ea51642da13eaa449b89bd970c
Florian GAMBARDELLA <florian.vente59@gmail.com>
2025-11-11 22:56:10 +0000

    const firebaseConfig = {   apiKey: "AIzaSyBwt7rELoo9C1hyi6xtYiUT7BIOMgWI

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 // IMPORTANT: Replace with your actual Firebase project configuration
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBwt7rELoo9C1hyi6xtYiUT7BIOMgWIiJA",
+  authDomain: "tyretrace-ia.firebaseapp.com",
+  projectId: "tyretrace-ia",
+  storageBucket: "tyretrace-ia.appspot.com",
+  messagingSenderId: "482093220627",
+  appId: "1:482093220627:web:a9a27c0ab2a6dc951750e6"
 };
